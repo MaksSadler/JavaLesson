aa64f3a408caf1fa0c611a78ef61a21a3f4e3f03
Max <max0sadler@gmail.com>
2023-04-05 21:40:56 +0300

    create Project5

---
+++ ConsoleApp/Project5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Max")
+{
+    Console.WriteLine("Yeaahhh!!!");    
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
