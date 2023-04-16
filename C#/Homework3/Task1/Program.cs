6f0411dedd949c33d9f3aa4194e1cc5f540635c3
Max <max0sadler@gmail.com>
2023-04-16 18:34:16 +0300

    create new homework3

---
+++ C#/Homework3/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string input = Console.ReadLine();
+int len = input.Length;
+
+if (len == 5)
+{
+    if (input[0] == input[4] && input[1] == input[3])
+    {
+        Console.WriteLine($"{input} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{input} - Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"{input} - не является пятизначным числом");
+}
---
+++ C#/Homework3/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число ");
+ int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+ int Y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+ int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+ int X2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+ int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+ int Z2 = Convert.ToInt32(Console.ReadLine()); 
+
+double d=Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2)+Math.Pow((Z2-Z1),2));
+Console.WriteLine(d);
