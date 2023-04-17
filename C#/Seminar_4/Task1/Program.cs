a2db8b1a94d9d7120fd19ef26f91386edbd49f4b
Max <max0sadler@gmail.com>
2023-04-17 20:53:32 +0300

    create new seminar

---
+++ C#/Seminar_4/Task1/Program.cs
@@ -0,0 +1,80 @@
+﻿//Вид 1
+// void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+//Method1();
+
+//Вид 2
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("Message text");
+
+//Вид 3
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+//Вид4
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result += text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "qwerty");
+// Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+int [] arr = {1, 5, 4, 3, 2, 6, 9, 1, 1,};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
