73d86b77947a7cd3100b874c8c6a2e9b037c4e0a
Max <max0sadler@gmail.com>
2023-04-22 12:31:39 +0300

    create homework5

---
+++ C#/Homework5/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿double[] numArr = new double[5];
+
+for(int i=0;i<numArr.Length;i++)
+{
+    numArr[i] = Convert.ToDouble(new Random().Next(1,1000))/100;
+}
+Console.WriteLine($"Массив значений: {String.Join(",",numArr)}");
+
+double min = numArr[0];
+double max = numArr[0];
+for (int i=0;i<numArr.Length;i++)
+{
+    if (numArr[i] < min)
+    {
+        min = numArr[i];   
+    }
+     if (numArr[i] > max)
+    {
+        max = numArr[i];   
+    }    
+}
+double a = max - min;
+
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine("Разница между максимальным и минимальным значением =  "+"{0:0.00}",a);
