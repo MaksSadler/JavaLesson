0748a0d957f344b7e370d520c695cb553f592f53
Max <max0sadler@gmail.com>
2023-04-29 10:57:11 +0300

    create new seminars and homeworks

---
+++ C#/Homework6/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] numArr = new int[n];
+
+Console.WriteLine("Введите " + n + " целых чисел через пробел:");
+string[] input = Console.ReadLine().Split(" ");
+
+int count = 0;
+for (int i = 0; i < numArr.Length; i++)
+{
+    numArr[i] = Convert.ToInt32(input[i]);
+    if (numArr[i] > 0)
+    {
+        count++;
+    }
+
+
+}
+Console.WriteLine("Количество чисел больше 0: " + count);
