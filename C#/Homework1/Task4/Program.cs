4e0303b46de055523994aad63dca9773879529e1
MaksSadler <128633467+MaksSadler@users.noreply.github.com>
2023-06-12 14:58:42 +0300

    Delete C# directory

--- C#/Homework1/Task4/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Ввелите число");
-int number = Convert.ToInt32(Console.ReadLine());
-int even = 1;
-while (even < number)
-{
-    Console.WriteLine(even+1);
-    even+=2;
-}
--- C#/Homework3/Task3/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Введите число ");
- int num = Convert.ToInt32(Console.ReadLine());
-
- for (int i=1; i<=num; i++)
- {
-    Console.WriteLine(Math.Pow(i,3));
- }
