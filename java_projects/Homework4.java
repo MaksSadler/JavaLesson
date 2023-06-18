03698e2c4099de2e1cb67f4c11e8c71fea50c9c2
Max <max0sadler@gmail.com>
2023-06-18 19:19:10 +0300

    create new Homework4

--- java_projects/Homework4.java
+++ java_projects/Homework4.java
@@ -28,12 +28,12 @@ public class Homework4 {
      * < []
      * > revert -> throw new NoSuchElementException
      */
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         LinkedList<String> list = new LinkedList<>();
 
         while (true) {
-            System.out.println("Введите строку :");
+            System.out.println("Введите строку:");
             String input = scanner.nextLine();
 
             if (input.equals("print")) {
@@ -45,10 +45,19 @@ public class Homework4 {
                 if (!list.isEmpty()) {
                     list.removeLast();
                     System.out.println("Последняя введенная строка удалена из памяти.");
+                }  try {
+                
+            } catch (NumberFormatException e) {
+                System.err.println(" ");
+                continue;
+            }
             } else {
                 list.addLast(input);
             }
+            String command = scanner.nextLine();
+            if ("exit".equals(command)) {
+                System.exit(0);
+            }
         }
-    }
     }
 }
