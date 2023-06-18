6e23344d61e2977743d2d003acc4f688753887fd
Max <max0sadler@gmail.com>
2023-06-18 18:43:13 +0300

    create new Homework4

---
+++ java_projects/Homework4.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class Homework4 {
+
+    /**
+     * Реализовать консольное приложение, которое:
+     * 1. Принимает от пользователя и “запоминает” строки.
+     * 2. Если введено print, выводит строки так, чтобы последняя введенная была
+     * первой в списке, а первая - последней.
+     * 3. Если введено revert, удаляет предыдущую введенную строку из памяти.
+     * 4. Если введено exit, то программа завершается
+     *
+     *
+     *
+     * > - ввод в консоль (stdin), < - вывод на консоль (stdout)
+     * > java
+     * > python
+     * > c#
+     * > print
+     * < [c#, python, java]
+     * > revert
+     * > print
+     * < [python, java]
+     * > revert
+     * > revert
+     * > print
+     * < []
+     * > revert -> throw new NoSuchElementException
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<String> list = new LinkedList<>();
+
+        while (true) {
+            System.out.println("Введите строку :");
+            String input = scanner.nextLine();
+
+            if (input.equals("print")) {
+                System.out.println("Строки в обратном порядке:");
+                while (!list.isEmpty()) {
+                    System.out.println(list.removeLast());
+                }
+            } else if (input.equals("revert")) {
+                if (!list.isEmpty()) {
+                    list.removeLast();
+                    System.out.println("Последняя введенная строка удалена из памяти.");
+            } else {
+                list.addLast(input);
+            }
+        }
+    }
+    }
+}
